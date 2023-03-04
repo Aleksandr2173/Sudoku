d7540618d319e6fbe6e9ece21fbd22767a39f0a2
Olga Plotnikova <plotnikowa_olya@list.ru>
2023-03-04 11:26:45 +0300

    prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -3,16 +3,13 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-}
+function solve(boardString) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -20,7 +17,22 @@ function isSolved(board) {
  * Подумай, как симпатичнее сформировать эту строку.
  */
 function prettyBoard(board) {
+  let result = [];
+  for (let y = 0; y < board.length; y += 1) {
+    const box = board[y].slice(0, 3);
+    if (y === 0) box.unshift("\n");
+    if (y === 3 || y == 6) box.unshift(" ", "\n");
+    box.push(
+      " ",
+      board[y].slice(3, 6).join(" "),
+      " ",
+      board[y].slice(6).join(" "),
+      "\n"
+    );
+    result.push(box.join(" "));
+  }
 
+  return result.join(" ");
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
